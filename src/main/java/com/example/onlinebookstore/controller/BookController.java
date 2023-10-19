fa8992a93a461b6041245403a4e1cdcfd9a90717
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-10-19 16:33:11 +0300

    made requested changes

--- src/main/java/com/example/onlinebookstore/controller/BookController.java
+++ src/main/java/com/example/onlinebookstore/controller/BookController.java
@@ -3,9 +3,8 @@ package com.example.onlinebookstore.controller;
 import com.example.onlinebookstore.dto.BookDto;
 import com.example.onlinebookstore.dto.CreateBookRequestDto;
 import com.example.onlinebookstore.service.BookService;
-import java.util.List;
-
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,7 +40,8 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    public BookDto updateBook(@RequestBody @Valid CreateBookRequestDto requestDto, @PathVariable Long id) {
+    public BookDto updateBook(@RequestBody @Valid CreateBookRequestDto requestDto,
+                              @PathVariable Long id) {
         return bookService.updateById(requestDto, id);
     }
 }
