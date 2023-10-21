729de5e2a8abb0cc896ee9b2b78143377e1dff54
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-10-21 18:52:32 +0300

    add id field to book dto

--- src/main/java/com/example/onlinebookstore/dto/BookDto.java
+++ src/main/java/com/example/onlinebookstore/dto/BookDto.java
@@ -6,6 +6,9 @@ import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 
 public record BookDto(
+        @NotNull
+        @PositiveOrZero
+        Long id,
         @NotBlank
         String title,
         @NotBlank
