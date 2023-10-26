633aeac4c28c7c761468efc88fff105714f7c7ed
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-10-26 14:12:29 +0300

    Add swagger annotations

--- src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.example.onlinebookstore.dto.user.UserResponseDto;
 import com.example.onlinebookstore.exceptions.RegistrationException;
 import com.example.onlinebookstore.security.AuthenticationService;
 import com.example.onlinebookstore.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication", description = "Endpoints for authentication")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/auth/")
@@ -23,11 +26,13 @@ public class AuthenticationController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Login", description = "Users can login using their credentials")
     @PostMapping("/login")
     public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
         return authenticationService.authenticate(request);
     }
 
+    @Operation(summary = "Register", description = "Users can create new account")
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
