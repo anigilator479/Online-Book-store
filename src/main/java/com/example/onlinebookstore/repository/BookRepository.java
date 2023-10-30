c5e734c834f854c34cf5f4fcb75c1b635e8ba463
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-10-30 16:48:51 +0300

    Add entityGraph annotation in book repository methods to prevent lazy init exception

--- src/main/java/com/example/onlinebookstore/repository/BookRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/BookRepository.java
@@ -2,10 +2,19 @@ package com.example.onlinebookstore.repository;
 
 import com.example.onlinebookstore.model.Book;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @EntityGraph(attributePaths = "categories")
+    Optional<Book> findById(Long id);
+    @EntityGraph(attributePaths = "categories")
+    Page<Book> findAll(Pageable pageable);
     List<Book> findAllByCategoriesId(Long categoryId);
 }
