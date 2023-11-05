b4cfa268c4f50e425e4b6eab4aa39f4391b16eeb
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-11-05 19:16:17 +0200

    add repository annotation in ShoppingCartRepository

--- src/main/java/com/example/onlinebookstore/repository/ShoppingCartRepository.java
+++ src/main/java/com/example/onlinebookstore/repository/ShoppingCartRepository.java
@@ -4,7 +4,9 @@ import com.example.onlinebookstore.model.ShoppingCart;
 import com.example.onlinebookstore.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
     Optional<ShoppingCart> findShoppingCartByUser(User user);
 }
